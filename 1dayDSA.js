7a6ec650030ebc9d92e9f8bd0bf007b4b70ec8dd
Mrlexcoder <mrlexcoder@gmail.com>
2024-05-24 23:46:59 +0530

    First

---
+++ 1dayDSA.js
@@ -0,0 +1,15 @@
+// console.log("Amit.sandhu")
+const accountId=12345
+let accountemail = "mrlexcoder@gmail.com"
+var accountPassword = "12345"
+accountcity = "INDIA"
+
+// accountId=14412  do not change
+
+console.log(accountId)
+
+accountPassword="322323"
+
+console.log(accountPassword)
+accountcity = "VABYBA"
+console.log(accountPassword)
