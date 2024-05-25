583dbdd7a45385c3e0345efefe64a54d5f4b5bd0
Mrlexcoder <mrlexcoder@gmail.com>
2024-05-25 23:41:33 +0530

    2nd day learn js

---
+++ 2daylearm.js
@@ -0,0 +1,10 @@
+// console.log("hello DEMO")
+const accountemail = "ALX@gmail.com"
+let accountnumber = 1234523
+var accountpassword = "8728882"
+
+let x=12;
+
+
+// in const we cant chage value
+console.table([accountnumber,accountpassword,accountpassword]);
